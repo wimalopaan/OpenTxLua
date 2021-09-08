e5247feede6060e712422fb36cef783245f3ec05
wimalopaan <wilhelm.wm.meier@googlemail.com>
2021-09-08 10:33:29 +0200

    improved schotl.lua to save last position as dead-position and export internal state for use with other switches

--- SCRIPTS/MIXES/schotl.lua
+++ SCRIPTS/MIXES/schotl.lua
@@ -1,6 +1,6 @@
 --
 -- WM OTXE - OpenTX Extensions 
--- Copyright (C) 2020 Wilhelm Meier <wilhelm.wm.meier@googlemail.com>
+-- Copyright (C) 2020, 2021 Wilhelm Meier <wilhelm.wm.meier@googlemail.com>
 --
 -- This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License. 
 -- To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/4.0/ 
@@ -10,27 +10,59 @@ local input = {
    {"Eing A1", SOURCE},
    {"Eing B1", SOURCE},
    {"Eing A2", SOURCE},
-   {"Eing B2", SOURCE}
+   {"Eing B2", SOURCE},
+   {"Deadband", VALUE, 1, 100, 10}, -- absolute value
+   {"Timeout ms", VALUE, 1, 300, 100} -- milli secs
 };
 
 local output = { "SchottPow 1",
 		 "SchottDir 1",
 		 "SchottPow 2",
-		 "SchottDir 2"
+		 "SchottDir 2",
+		 "State 1",
+		 "State 2"
 };
 
-local function run(a1, b1, a2, b2)
+local lastDirs = {
+   {0, 0},
+   {0, 0}
+};
+
+local nextRunTime = 0;
+local function saveLastDir(index, value, timeout)
+   if (getTime() > nextRunTime) then
+      nextRunTime = getTime() + timeout;
+
+      lastDirs[index][1] = lastDirs[index][2];
+      lastDirs[index][2] = value;
+   end
+end
+
+local function run(a1, b1, a2, b2, deadband, to)
+   local state1 = 0;
+   local state2 = 0;
+   local timeout = to / 10;
    local SchottelPow1 = math.sqrt(a1 * a1 + b1 * b1) / 1.41421;
    local SchottelDir1 = 0;
-   if (SchottelPow1 > 10) then
+   if (SchottelPow1 > deadband) then
       SchottelDir1 = math.atan2(b1, a1) * 1024 / math.pi;
+      saveLastDir(1, SchottelDir1, timeout);
+      state1 = 1;
+   else
+      SchottelDir1 = lastDirs[1][1];
+      state1 = 0;
    end
    local SchottelPow2 = math.sqrt(a2 * a2 + b2 * b2) / 1.41421;
    local SchottelDir2 = 0;
-   if (SchottelPow2 > 10) then
+   if (SchottelPow2 > deadband) then
       SchottelDir2 = math.atan2(b2, a2) * 1024 / math.pi;
+      saveLastDir(2, SchottelDir2, timeout);
+      state2 = 1;
+   else
+      SchottelDir2 = lastDirs[2][1];
+      state2 = 0;
    end
-   return SchottelPow1, SchottelDir1, SchottelPow2, SchottelDir2;
+   return SchottelPow1, SchottelDir1, SchottelPow2, SchottelDir2, state1, state2;
 end
 
 return {input=input, run=run, output=output}
